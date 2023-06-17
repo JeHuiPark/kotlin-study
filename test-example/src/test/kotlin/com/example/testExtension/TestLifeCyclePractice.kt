ae5009e18cae3454ed7f2b68474917cb24958f69
jehuipark <pjh2359@gmail.com>
2023-06-17 17:54:21 +0900

    Refactor testExtension practice classes

--- test-example/src/test/kotlin/com/example/testExtension/TestLifeCyclePractice.kt
+++ test-example/src/test/kotlin/com/example/testExtension/TestLifeCyclePractice.kt
@@ -1,10 +1,33 @@
 package com.example.testExtension
 
 import com.example.testExtension.TestLifeCyclePractice.Fixture.printTestScope
+import io.kotest.core.annotation.AutoScan
+import io.kotest.core.listeners.AfterContainerListener
+import io.kotest.core.listeners.AfterEachListener
+import io.kotest.core.listeners.AfterInvocationListener
+import io.kotest.core.listeners.AfterProjectListener
+import io.kotest.core.listeners.AfterSpecListener
+import io.kotest.core.listeners.AfterTestListener
+import io.kotest.core.listeners.BeforeContainerListener
+import io.kotest.core.listeners.BeforeEachListener
+import io.kotest.core.listeners.BeforeInvocationListener
+import io.kotest.core.listeners.BeforeProjectListener
+import io.kotest.core.listeners.BeforeSpecListener
+import io.kotest.core.listeners.BeforeTestListener
+import io.kotest.core.listeners.FinalizeSpecListener
+import io.kotest.core.listeners.PrepareSpecListener
+import io.kotest.core.spec.Spec
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.core.test.TestCase
+import io.kotest.core.test.TestResult
 import io.kotest.core.test.TestScope
+import kotlin.reflect.KClass
 
 /**
+ * Print Result
+ *
+ * ```
+ * beforeProject
  * TestLifeCyclePractice: prepareSpec
  * TestLifeCyclePractice: beforeSpec
  * 1 level: beforeContainer
@@ -39,6 +62,8 @@ import io.kotest.core.test.TestScope
  * multiple invocation: afterEach
  * TestLifeCyclePractice: afterSpec
  * TestLifeCyclePractice: finalizeSpec
+ * afterProject
+ * ```
  */
 class TestLifeCyclePractice : FreeSpec({
 
@@ -58,6 +83,88 @@ class TestLifeCyclePractice : FreeSpec({
 
         fun TestScope.printTestScope() {
             println("${testCase.name.testName} block")
+        }
+    }
+
+    @AutoScan
+    object ExtensionPractice :
+        BeforeContainerListener,
+        AfterContainerListener,
+        BeforeEachListener,
+        AfterEachListener,
+        BeforeTestListener,
+        AfterTestListener,
+        BeforeInvocationListener,
+        AfterInvocationListener,
+        BeforeSpecListener,
+        AfterSpecListener,
+        PrepareSpecListener,
+        FinalizeSpecListener,
+        BeforeProjectListener,
+        AfterProjectListener {
+
+        override suspend fun beforeSpec(spec: Spec) {
+            println("${spec.javaClass.simpleName}: beforeSpec")
+        }
+
+        override suspend fun afterSpec(spec: Spec) {
+            println("${spec.javaClass.simpleName}: afterSpec")
+        }
+
+        override suspend fun beforeTest(testCase: TestCase) {
+            println("${testCase.name.testName}: beforeTest")
+        }
+
+        override suspend fun afterTest(testCase: TestCase, result: TestResult) {
+            println("${testCase.name.testName}: afterTest")
+        }
+
+        override suspend fun beforeContainer(testCase: TestCase) {
+            println("${testCase.name.testName}: beforeContainer")
+        }
+
+        override suspend fun afterContainer(testCase: TestCase, result: TestResult) {
+            println("${testCase.name.testName}: afterContainer")
+        }
+
+        override suspend fun beforeEach(testCase: TestCase) {
+            println("${testCase.name.testName}: beforeEach")
+        }
+
+        override suspend fun afterEach(testCase: TestCase, result: TestResult) {
+            println("${testCase.name.testName}: afterEach")
+        }
+
+        override suspend fun beforeInvocation(testCase: TestCase, iteration: Int) {
+            println("${testCase.name.testName}: beforeInvocation, iteration count: $iteration")
+        }
+
+        override suspend fun afterInvocation(testCase: TestCase, iteration: Int) {
+            println("${testCase.name.testName}: afterInvocation, iteration count: $iteration")
+        }
+
+        override suspend fun beforeAny(testCase: TestCase) {
+            println("${testCase.name.testName}: beforeAny")
+        }
+
+        override suspend fun afterAny(testCase: TestCase, result: TestResult) {
+            println("${testCase.name.testName}: afterAny")
+        }
+
+        override suspend fun finalizeSpec(kclass: KClass<out Spec>, results: Map<TestCase, TestResult>) {
+            println("${kclass.simpleName}: finalizeSpec")
+        }
+
+        override suspend fun prepareSpec(kclass: KClass<out Spec>) {
+            println("${kclass.simpleName}: prepareSpec")
+        }
+
+        override suspend fun beforeProject() {
+            println("beforeProject")
+        }
+
+        override suspend fun afterProject() {
+            println("afterProject")
         }
     }
 }
